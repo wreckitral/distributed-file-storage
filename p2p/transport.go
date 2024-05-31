7df791a43b1626cb9fca89d892180aabd97bd46a
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-31 08:07:32 +0700

    feat: create transport for tcp

--- p2p/transport.go
+++ p2p/transport.go
@@ -1,4 +1,15 @@
 package p2p
 
+// Peer is an interface that represents the remote node
+type Peer interface {
+
+}
+
+// Transport is anything that handle communication between nodes
+// this can be of the form (TCP, UDP, websockets...)
+type Transport interface {
+
+}
+
 
 
