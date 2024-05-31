aae6900307b34691ca85a9472c62314f0d6bae94
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-31 09:43:21 +0700

    chore: run TCP connection

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+
+	"github.com/wreckitral/distributed-file-storage/p2p"
+)
+
+func main() {
+    tr := p2p.NewTCPTransport(":7000")
+
+    if err := tr.ListenAndAccept(); err != nil {
+        log.Fatal(err)
+    }
+
+    select {}
+}
