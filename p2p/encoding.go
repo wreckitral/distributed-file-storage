ceba0e3a553b6792754a6fb2545e64754872e4e7
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-08 22:08:27 +0700

    fix(message): change naming

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -6,14 +6,14 @@ import (
 )
 
 type Decoder interface {
-    Decode(io.Reader, *Message) error
+    Decode(io.Reader, *RPC) error
 }
 
 type GOBDecoder struct {
 
 }
 
-func (dec GOBDecoder) Decode(r io.Reader, msg *Message) error  {
+func (dec GOBDecoder) Decode(r io.Reader, msg *RPC) error  {
     return gob.NewDecoder(r).Decode(msg)
 }
 
@@ -21,7 +21,7 @@ type DefaultDecoder struct {
 
 }
 
-func (dec DefaultDecoder) Decode(r io.Reader, msg *Message) error  {
+func (dec DefaultDecoder) Decode(r io.Reader, msg *RPC) error  {
     buf := make([]byte, 1028)
     n, err := r.Read(buf)
     if err != nil {
--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -84,15 +84,15 @@ func (t *TCPTransport) handleConn(conn net.Conn) {
     }
     
     // Read loop
-    msg := &Message{}
+    rpc := &RPC{}
     for {
-        if err := t.Decoder.Decode(conn, msg); err != nil {
+        if err := t.Decoder.Decode(conn, rpc); err != nil {
             fmt.Printf("tcp error: %s\n", err)
             continue
         }
 
-        msg.From = conn.RemoteAddr()
+        rpc.From = conn.RemoteAddr()
 
-        fmt.Printf("message:%+v\n", msg)
+        fmt.Printf("message:%+v\n", rpc)
     }
 }
